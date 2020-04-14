f9fef5f9bde7d178ec2ba7cce3b624f85baca1a3
Derek Borski <derek.borski@gmail.com>
2020-04-13 22:59:13 -0600

    Adds total_cost method

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -25,6 +25,19 @@ class ActivityTest < Minitest::Test
 
     expected = {"Maria" => 20}
     assert_equal expected, @activity.participants
+
+    @activity.add_participant("Luther", 40)
+
+    expected2 = {"Maria" => 20, "Luther" => 40}
+    assert_equal expected2, @activity.participants
+  end
+
+  def test_total_cost
+    @activity.add_participant("Maria", 20)
+    assert_equal 20, @activity.total_cost
+
+    @activity.add_participant("Luther", 40)
+    assert_equal 60, @activity.total_cost
   end
 end
 
